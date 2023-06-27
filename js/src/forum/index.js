55abdcc29ebf63c973390eb00c475bc9082eccc8
Ian Morland <ian@morland.me>
2023-06-27 11:49:12 +0100

    feat: support for forgot password and login modals

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -3,6 +3,8 @@ import DiscussionComposer from 'flarum/forum/components/DiscussionComposer';
 import ReplyComposer from 'flarum/forum/components/ReplyComposer';
 import extendSignUp from './extendSignUp';
 import extendComposer from './extendComposer';
+import addRecaptchaToLogin from './addRecaptchaToLogin';
+import addRecaptchaToForgotPassword from './addRecaptchaToForgotPassword';
 
 app.initializers.add('fof/recaptcha', () => {
   app.recaptchaLoaded = false;
@@ -10,4 +12,6 @@ app.initializers.add('fof/recaptcha', () => {
   extendSignUp();
   extendComposer(DiscussionComposer);
   extendComposer(ReplyComposer);
+  addRecaptchaToLogin();
+  addRecaptchaToForgotPassword();
 });
