d54c84411fceb26b380bde87e2517ece0a19e41d
David Sevilla Martin <me@dsevilla.dev>
2023-07-01 12:14:28 -0400

    Apply same logic from 'SignUpModal' to 'LogInModal' and 'ForgotPasswordModal'
    Also refactors code to become callable (similar to 'extendComposer') for each modal.
    Resolves issues with invisible reCAPTCHA in those two modals; fixes #39

---
+++ js/src/forum/extendAuthModals.tsx
@@ -0,0 +1,81 @@
+import app from 'flarum/forum/app';
+import ForgotPasswordModal from 'flarum/forum/components/ForgotPasswordModal';
+import LogInModal from 'flarum/forum/components/LogInModal';
+import SignUpModal from 'flarum/forum/components/SignUpModal';
+import { extend, override } from 'flarum/common/extend';
+
+import RecaptchaState from './states/RecaptchaState';
+import Recaptcha from './components/Recaptcha';
+
+export const addRecaptchaToAuthModal = <T extends typeof ForgotPasswordModal | typeof LogInModal | typeof SignUpModal>({
+  modal,
+  type,
+  dataMethod,
+}: {
+  modal: T;
+  type: string;
+  dataMethod: string;
+}) => {
+  const isInvisible = app.data['fof-recaptcha.type'] === 'invisible';
+  const isEnabled = () => !!app.forum.attribute(`fof-recaptcha.${type}`);
+
+  extend(modal.prototype, 'oninit', function () {
+    if (!isEnabled()) return;
+
+    this.recaptcha = new RecaptchaState(
+      () => {
+        if (isInvisible) {
+          // Create "fake" event so this works when other extensions extend onsubmit as well
+          const event = new Event('submit');
+          event.isRecaptchaSecondStep = true;
+          this.onsubmit(event);
+        }
+      },
+      (alertAttrs) => {
+        // Removes the spinner on the submit button so we can try again
+        this.loaded();
+        this.alertAttrs = alertAttrs;
+      }
+    );
+  });
+
+  extend(modal.prototype, dataMethod, function (data) {
+    if (!isEnabled()) return;
+
+    data['g-recaptcha-response'] = this.recaptcha.getResponse();
+  });
+
+  extend(modal.prototype, 'fields', function (fields) {
+    if (!isEnabled()) return;
+
+    fields.add('recaptcha', <Recaptcha state={this.recaptcha} />, -5);
+  });
+
+  extend(modal.prototype, 'onerror', function () {
+    if (!isEnabled()) return;
+
+    this.recaptcha.reset();
+  });
+
+  override(modal.prototype, 'onsubmit', function (original, e) {
+    if (!isEnabled()) return;
+
+    if (isInvisible && !e.isRecaptchaSecondStep) {
+      // When recaptcha is invisible, onsubmit will be called two times
+      // First time with normal event, we will call recaptcha.execute
+      // Second time is called from recaptcha callback with a special isRecaptcha attribute
+      e.preventDefault();
+      this.loading = true;
+      this.recaptcha.execute();
+      return;
+    }
+
+    return original(e);
+  });
+};
+
+export default () => {
+  addRecaptchaToAuthModal({ modal: ForgotPasswordModal, type: 'forgot', dataMethod: 'requestParams' });
+  addRecaptchaToAuthModal({ modal: LogInModal, type: 'signin', dataMethod: 'loginParams' });
+  addRecaptchaToAuthModal({ modal: SignUpModal, type: 'signup', dataMethod: 'submitData' });
+};
--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,17 +1,13 @@
 import app from 'flarum/forum/app';
 import DiscussionComposer from 'flarum/forum/components/DiscussionComposer';
 import ReplyComposer from 'flarum/forum/components/ReplyComposer';
-import extendSignUp from './extendSignUp';
 import extendComposer from './extendComposer';
-import addRecaptchaToLogin from './addRecaptchaToLogin';
-import addRecaptchaToForgotPassword from './addRecaptchaToForgotPassword';
+import extendAuthModals from './extendAuthModals';
 
 app.initializers.add('fof/recaptcha', () => {
   app.recaptchaLoaded = false;
 
-  extendSignUp();
   extendComposer(DiscussionComposer);
   extendComposer(ReplyComposer);
-  addRecaptchaToLogin();
-  addRecaptchaToForgotPassword();
+  extendAuthModals();
 });
