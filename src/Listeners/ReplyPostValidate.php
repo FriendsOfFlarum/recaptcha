d7cafcd0628c7ab844971ae9fd9fc66ec08dffc2
David Sevilla Martin <me@dsevilla.dev>
2023-08-11 12:43:59 -0400

    Fix reply post validation not properly checking for first post

--- src/Listeners/ReplyPostValidate.php
+++ src/Listeners/ReplyPostValidate.php
@@ -36,7 +36,7 @@ class ReplyPostValidate
             // If it's a new discussion, the reCAPTCHA is already validated in discussion saving event
             // When this code runs, the discussion already exists, and the number has not been assigned to the post yet
             // So we look in the discussion number index, just like the reply permission check does in PostReplyHandler
-            if ($event->post->discussion->post_number_index === 0) {
+            if ($event->post->number == 1 || $event->post->discussion->first_post_id === null) {
                 return;
             }
 
